704349246ad1e48871f5459b10f231a6f887b2f5
Durojaiye Segun Azeez <sEgZy@MacBook-Pro-3.broadband>
2024-03-08 05:41:51 +0000

    cy-origin fixed url

--- cypress/e2e/test/index.spec.cy.js
+++ cypress/e2e/test/index.spec.cy.js
@@ -1,9 +1,14 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('testing cross origin', () => {
+  it('cross origin journey', () => {
     cy.visit('/')
     cy.get('[href="http://elementalselenium.com/"]')
-    .should('be.visible').invoke('removeAttr', 'target').click()
-    cy.get('#email').should('be.visible').type('handsomezy@gmail.com')
-    cy.get('select').select('JavaScript')
+    .invoke('removeAttr', 'target').click()
+    cy.origin('https://elementalselenium.com/', ()=>{
+      cy.log('new tab load is successful')
+      cy.get('#email').should('be.visible').type('handsomezy@gmail.com')
+      cy.get('select').select('JavaScript')
+    })
+    // cy.get('#email').should('be.visible').type('handsomezy@gmail.com')
+    // cy.get('select').select('JavaScript')
   })
 })
